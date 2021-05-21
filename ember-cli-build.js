e078a754fb7853cc9f585786305665e7ee205406
rajendki <Kishore.rajendran@freshworks.com>
2021-05-21 10:15:11 +0530

    initial commit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    outputPaths: {
+      app: {
+        css: {
+          'consult': '/app/styles/_consult.css'
+        }
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
